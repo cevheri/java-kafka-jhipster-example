cab57ed0143c110daa59108a7011192f7105e2a0
Cevheri <cevheribozoglan@gmail.com>
2021-07-12 10:30:29 +0300

    Initial version of jkfk generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/cevher/kfk/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cevher.kfk.repository;
+
+import com.cevher.kfk.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/cevher/kfk/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.cevher.kfk.web.rest;
